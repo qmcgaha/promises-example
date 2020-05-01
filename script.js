bf5602bbc958fd275940741f0a37d55d57d0c075
Qmcgaha <qmcgaha@gmail.com>
2020-05-01 08:10:40 -0500

    Another test

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let promise = fetch("coffee.jpg");
+let promise = fetch("cofee.jpg");
 let promise2 = promise.then(response => response.blob());
 let promise3 = promise2.then(myBlob => {
-  let objectURL = UL.createObjectURL(myBlob);
+  let objectURL = URL.createObjectURL(myBlob);
   let image = document.createElement("img");
   image.src = objectURL;
   document.body.appendChild(image);
